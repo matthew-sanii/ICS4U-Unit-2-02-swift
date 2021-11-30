1c1671d5d03de46f36f03076cf946a54b394bee0
Ubuntu <ubuntu@ip-172-31-89-75.ec2.internal>
2021-11-30 21:26:40 +0000

    edited Factorial.swift

--- Factorial.swift
+++ Factorial.swift
@@ -4,16 +4,16 @@ func factorial(value: Double) -> Double {
     let step = value
     if step == 0 {
         return 1
-    }
-    else {
-        return(factorial(value:value - 1) * value)
+    } else {
+        return(factorial(value: value - 1) * value)
     }
 }
 
 print("Input what you are using facorial on: ")
 let usrInput = readLine()!
-guard var base = Double(usrInput) else { 
-    fatalError("expected a number got something else.")
+guard var base = Double(usrInput) else {
+    print("Expected a number got something else.")
+    exit(0)
 }
 base = round(base)
 let answer = factorial(value: base)
